b3cf9a07def6eccb4e7dcd05477a0613363bdc1a
Dmitry Vibe <riim@yandex.ru>
2015-07-11 12:34:23 +0600

    v1.0

--- index.js
+++ index.js
@@ -1,2 +1,30 @@
+var path = require('path');
 
-module.exports = require('./src/htmlBindify');
+var through = require('through');
+var htmlBindingTransform = require('html-binding-transform');
+
+function configure(exts, opts) {
+	return function(file) {
+		if (exts.indexOf(path.extname(file)) == -1) {
+			return through();
+		}
+
+		var input = '';
+
+		function write(buf) {
+			input += buf;
+		}
+
+		function end() {
+			this.queue(htmlBindingTransform(input, opts));
+			this.queue(null);
+		}
+
+		return through(write, end);
+	};
+}
+
+var htmlBindify = configure(['.html']);
+htmlBindify.configure = configure;
+
+module.exports = htmlBindify;
